3f7924aad371aba4223aca21978b81e363843cc5
prakash <prakash.chaurasiya214522@gmail.com>
2025-09-23 18:32:33 +0530

    Dsa note

--- java/DSA_Java/src/Recursion/TowerOfHanoi.java
+++ java/DSA_Java/src/Recursion/TowerOfHanoi.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 class Demo21{
 	static void towerOfHanoi(int n, String src, String helper, String dest) {
 		if(n==1) {
-			System.out.println("Move The Disk "+n+" from "+src+"to "+dest);
+			System.out.println("Move The Disk "+n+"  from "+src+"to "+dest);
 			return;
 		}
 		towerOfHanoi(n-1,src,dest,helper);
